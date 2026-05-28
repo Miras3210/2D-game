c78c33c7beb64858e5dd00212df97603b7a99421
Railerival <raiabhimanyu09@gmail.com>
2026-05-28 17:39:48 +0530

    just cleaning the assets

--- Assets/Tree and bushes.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Tree and bushes" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="Tree Full spritesheet1.png" width="64" height="64"/>
-</tileset>
